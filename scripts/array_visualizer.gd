9bae5cbc26e292de7766d3893b02e4589eb22514
Gustavo Ordoñez <gustavoalberto117@gmail.com>
2025-08-24 02:14:20 -0300

    update: adjust text position

--- scripts/array_visualizer.gd
+++ scripts/array_visualizer.gd
@@ -1,7 +1,18 @@
 extends Node2D
 
+# Arrays
 var array: Array = []
 var highlight: Array = []
+
+# settings
+var bar_width: int = 40
+var bar_spacing: int = 5
+var base_y: int = 350
+var font := ThemeDB.fallback_font
+var height_scale = 20
+
+#Colors
+
 
 func set_array(new_array: Array) -> void:
 	array = new_array
@@ -12,12 +23,9 @@ func set_highlight(new_highlight: Array) -> void:
 	queue_redraw()
 
 func _draw() -> void:
-	var width = 40
-	var base_y = 350
-	var font := ThemeDB.fallback_font
-	var height_scale = 20
 	
 	# ancho total del gráfico
+	var width = bar_width + bar_spacing
 	var total_width = array.size() * width
 	# offset para centrar (asumiendo que tu ventana mide 800px de ancho)
 	var screen_width = get_viewport_rect().size.x
@@ -43,8 +51,9 @@ func _draw() -> void:
 
 		# Dibujar valor arriba de la barra
 		#draw_string(font, Vector2(i * width + 5, base_y - height - 5), str(array[i]), 0, -1, 16, Color(0.8, 0.9, 1))
-		draw_string(font, Vector2(x + 5, y - 5), str(array[i]), 0, -1, 16, Color(0.8, 0.9, 1))
+		#draw_string(font, Vector2(x + 5, y - 5), str(array[i]), 0, -1, 16, text_color)
+		draw_string(font, Vector2(x + bar_width/2 - bar_spacing, y - 5), str(array[i]), 0, -1, 16, text_color)
 		
 		# Dibujar índice debajo de la barra
 		#draw_string(font, Vector2(i * width + 5, base_y + 20), str(i),0, -1, 16, text_color)
-		draw_string(font, Vector2(x + 5, base_y + 20), str(i),0, -1, 16, text_color)
+		draw_string(font, Vector2(x + bar_width/2 - bar_spacing, base_y + 20), str(i),0, -1, 16, text_color)
